27548952f56242cd9701a7958e26edd9e9975d76
John-Bruce Noad <johnnoad04@gmail.com>
2022-11-09 21:38:56 +0200

    Added () and √, Red Buttons, Reduced Lines of JS

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ const previousOperand = document.querySelector('[data-previous-operand]');
 const currentOperand = document.querySelector('[data-current-operand]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const deleteButton = document.querySelector('[data-delete]');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const numberButtons = document.querySelectorAll('[data-number]');
-const exponentButton = document.querySelector('[data-exponent]');
+// const operationButtons = document.querySelectorAll('[data-operation]');
+// const numberButtons = document.querySelectorAll('[data-number]');
+const mathButtons = document.querySelectorAll('[data-operation], [data-number]');
 const equalsButton = document.querySelector('[data-equals]');
 let calculationMemory = previousOperand.textContent;
 
@@ -15,26 +15,21 @@ allClearButton.addEventListener('click', () => {
 })
 deleteButton.addEventListener('click', () => {
   calculationMemory = calculationMemory.slice(0, -1)
-  currentOperand.textContent = calculationMemory
+  logToCurrent()
 })
-operationButtons.forEach((button) => {
+mathButtons.forEach((button) => {
   button.addEventListener('click', () => {
     calculationMemory += `${button.textContent}`
-    currentOperand.textContent = calculationMemory
+  logToCurrent()
   })
-})
-numberButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    calculationMemory += `${button.textContent}`
-    currentOperand.textContent = calculationMemory
-  })
-})
-exponentButton.addEventListener('click', () => {
-  calculationMemory += `**`
-  currentOperand.textContent = calculationMemory
 })
 equalsButton.addEventListener('click', () => {
+  let expression = calculationMemory.replaceAll('√', '**(1/2)').replaceAll('^', '**').replaceAll('×', '*').replaceAll('÷', '/')
+  let result = eval(expression) 
   previousOperand.textContent = calculationMemory
-  currentOperand.textContent = eval(calculationMemory)
-  calculationMemory =  eval(calculationMemory).toString()
+  currentOperand.textContent = result
+  calculationMemory =  result.toString()
 })
+function logToCurrent() {
+  currentOperand.textContent = calculationMemory.replaceAll('**', '^')
+}
