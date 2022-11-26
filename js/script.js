6e57c1de94d752b81a89075b8eb864811ebd41d1
John-Bruce Noad <johnnoad04@gmail.com>
2022-11-26 18:58:39 +0200

    Updated Colour Styling & Error Message

--- js/script.js
+++ js/script.js
@@ -65,25 +65,24 @@ decimal.addEventListener('click', () => {
 equalsButton.addEventListener('click', () => {
   // Replace all Special Characters in memory string, with characters that JS can calculate with.
   let expression = calculationMemory.replaceAll('√', '').replaceAll('^', '**').replaceAll('×', '*').replaceAll('÷', '/').replaceAll('_', ')**(1/2)')
-  // Evaluating the expression from the memory
+  
+  // Evalutation and Error Handling
   try {
     let result = JSON.stringify(eval(expression))
     if (result.includes('.')) {
       decimalAllowed = false
     } else decimalAllowed = true
-
-    // Setting precious operation on the display to the current operand
     previousOperand.textContent = currentOperand.textContent
-    // Changing current Operand on display to the result of the calculation, and saving it to the memory
+
     if (result == 'null') {
-      currentOperand.textContent = "ERROR: CAN'T DIVIDE BY ZERO"
+      currentOperand.textContent = "ERROR: RESULT IS UNDEFINED"
     } else {
       currentOperand.textContent = result
       calculationMemory =  result
     }
 
   } catch (e){
-    console.log(e);
+    // console.log(e);
     previousOperand.textContent = currentOperand.textContent
     currentOperand.textContent = 'ERROR: INVALID SYNTAX'
   }
